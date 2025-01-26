68fb4b67c4bf4c453fdc9fd76f58d4c50b1e2482
minimatime <nvalluri3@gatech.edu>
2025-01-26 13:20:04 -0500

    initialized color set, 29 colors, changes made to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <div
-      className={`flex flex-col h-screen`}
+      className={`flex flex-col h-screen bg-brand-fill text-grey-text-strong`}
     >
-      <div className="flex items-center justify-center w-full h-[7%] border-solid border-b-2 mb-4">
+      <div className="flex items-center justify-center w-full h-[7%] border-brand-fill border-solid border-b-2 mb-4 bg-gradient-to-r from-brand-text from-30% via-brand-stroke-weak via-45% to-brand-stroke-strong to-60%">
         <h1>Bits of Good Design System Component Overview</h1>
       </div>
       <div className="flex h-[95%] w-[100%] justify-between pb-4 px-8">
@@ -11,28 +11,28 @@ export default function Home() {
           {/* 
             Put all switches here. Make sure all of the ones in Figma are here and match the design in Figma.
           */}
-          <div className="flex flex-col items-center justify-between border-solid border-2">
+          <div className="flex flex-col items-center justify-between border-grey-stroke-strong border-solid border-2">
             <h3 className="self-start ml-4">Switches:</h3>
           </div>
 
           {/* 
             Put all icons here. Make sure all of the ones in Figma are here and match the design in Figma.
           */}
-          <div className="flex flex-col items-center justify-between border-solid border-2">
+          <div className="flex flex-col items-center justify-between border-grey-stroke-strong border-solid border-2">
             <h3 className="self-start ml-4">Icons:</h3>
           </div>
 
           {/* 
             Put all buttons here. Make sure all of the ones in Figma are here and match the design in Figma.
           */}
-          <div className="flex flex-col items-center justify-between border-solid border-2">
+          <div className="flex flex-col items-center justify-between border-grey-stroke-strong border-solid border-2">
             <h3 className="self-start ml-4">Buttons:</h3>
           </div>
 
           {/* 
             Put all checkboxes here. Make sure all of the ones in Figma are here and match the design in Figma.
           */}
-          <div className="flex flex-col items-center justify-between border-solid border-2">
+          <div className="flex flex-col items-center justify-between border-grey-stroke-strong border-solid border-2">
             <h3 className="self-start ml-4">Checkboxes:</h3>
           </div>
         </div>
@@ -40,14 +40,14 @@ export default function Home() {
           {/* 
             Put all dropdown and dropdown options here. Make sure all of the ones in Figma are here and match the design in Figma.
           */}
-          <div className="flex flex-col items-center justify-between border-solid border-2">
+          <div className="flex flex-col items-center justify-between border-grey-stroke-strong border-solid border-2">
             <h3 className="self-start ml-4">Dropdowns:</h3>
           </div>
 
           {/* 
             Put all form elements here. Make sure all of the ones in Figma are here and match the design in Figma.
           */}
-          <div className="flex flex-col items-center justify-between border-solid border-2">
+          <div className="flex flex-col items-center justify-between border-grey-stroke-strong border-solid border-2">
             <h3 className="self-start ml-4">Forms:</h3>
           </div>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,44 @@ export default {
         'mobile': '640px',
         'desktop': '1024px',
       },
+      colors: {
+        'brand-text': '#FC5B43',
+        'brand-stroke-strong': '#FC5B43CC',
+        'brand-stroke-weak': '#FC5B4333',
+        'brand-fill': '#FC5B430D',
+
+        'grey-text-strong': '#22070B',
+        'grey-text-weak': '#22070BB2',
+        'grey-stroke-strong': '#22070B80',
+        'grey-off-state': '#22070B26',
+        'grey-stroke-weak': '#F9F9F9',
+        'grey-fill-weak': '#22070B0D',
+        'grey-fill-weaker': '#22070B06',
+
+        'status-blue-text': '#325CE8',
+        'status-blue-stroke-strong': '#325CE8CC',
+        'status-blue-stroke-weak': '#325CE833',
+        'status-blue-fill': '#325CE80D',
+
+        'status-green-text': '#0A7B40',
+        'status-green-stroke-strong': '#0A7B40CC',
+        'status-green-stroke-weak': '#0A7B4033',
+        'status-green-fill': '#0A7B400D',
+
+        'status-amber-text': '#8F6C1A',
+        'status-amber-stroke-strong': '#8F6C1ACC',
+        'status-amber-stroke-weak': '#8F6C1A33',
+        'status-amber-fill': '#8F6C1A0D',
+
+        'status-red-text': '#C73A3A',
+        'status-red-stroke-strong': '#C73A3ACC',
+        'status-red-stroke-weak': '#C73A3A33',
+        'status-red-fill': '#C73A3A0D',
+
+        'solid-bg-sunken': '#F9F9F9',
+        'solid-bg-base': '#FFFFFF',
+
+      },
     },
   },
   plugins: [],
