d380d7cbbd8781028c5b600de56a2a3c8f951192
Nathan Papa <nathanpapa35@gmail.com>
2025-02-20 01:26:32 -0500

    Fix hover

--- src/components/BogRadioItem/BogRadioItem.tsx
+++ src/components/BogRadioItem/BogRadioItem.tsx
@@ -15,7 +15,7 @@ export function BogRadioItem({ label, value, disabled = false }: BogRadioItemPro
       <RadioGroup.Item className={styles.radio} value={value} disabled={disabled} id={id}>
         <RadioGroup.Indicator className={styles.indicator} />
       </RadioGroup.Item>
-      <label className={styles.label} htmlFor={id} data-disabled={disabled}>
+      <label className={styles.label} data-disabled={disabled}>
         {label}
       </label>
     </div>
