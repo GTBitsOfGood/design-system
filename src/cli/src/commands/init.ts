b472a6247ac1b6c1063254f6a1c8d6fd04e3ca7d
Rohan Godha <git@rohangodha.com>
2025-03-01 00:54:36 -0500

    init command done

--- src/cli/src/commands/init.ts
+++ src/cli/src/commands/init.ts
@@ -1,8 +1,119 @@
 import { Command } from 'commander';
+import { existsSync } from 'fs';
+import { mkdir, writeFile } from 'fs/promises';
+import prompts from 'prompts';
+import { execSync } from 'child_process';
+import path from 'path';
+
+const DEV_DEPENDENCIES = ['tailwindcss', '@tailwindcss/postcss'];
+const DEPENDENCIES = [
+  '@phosphor-icons/react',
+  '@radix-ui/react-checkbox',
+  '@radix-ui/react-icons',
+  '@radix-ui/themes',
+  'phosphor-react',
+  'radix-ui',
+];
 
 export const init = new Command()
   .command('init')
   .description('Initialize a new project')
-  .action(() => {
-    console.log('Initializing the project');
+  .action(async () => {
+    const { root } = await prompts([
+      {
+        type: 'text',
+        name: 'root',
+        message: 'Where is the root of your project?',
+        initial: './',
+      },
+    ]);
+    if (!existsSync(root)) {
+      console.error('The root directory does not exist.');
+      return;
+    }
+
+    const { installDeps } = await prompts({
+      name: 'installDeps',
+      type: 'confirm',
+      message: 'Do you want to install dependencies?',
+      initial: true,
+    });
+    if (!installDeps) {
+      console.error('Skipping dependencies. You will not be able to use the design system without additional setup.');
+    } else {
+      const { packageManager } = await prompts({
+        type: 'select',
+        name: 'packageManager',
+        message: 'Which package manager do you want to use?',
+        choices: [
+          // the value is the command to run to install a package
+          { title: 'npm', value: 'npm install' },
+          { title: 'yarn', value: 'yarn add' },
+          { title: 'pnpm', value: 'pnpm add' },
+          { title: 'bun', value: 'bun add' },
+        ],
+      });
+
+      console.log('installing dependencies...');
+      execSync(`${packageManager} -D ${DEV_DEPENDENCIES.join(' ')}`);
+      execSync(`${packageManager} ${DEPENDENCIES.join(' ')}`);
+    }
+
+    const { setupTailwind } = await prompts({
+      name: 'setupTailwind',
+      type: 'confirm',
+      message: 'Do you want to set up Tailwind?',
+      initial: true,
+    });
+
+    if (setupTailwind) {
+      console.log('Setting up Tailwind v4 for Next.js...');
+      await writeFile(
+        path.join(root, 'postcss.config.mjs'),
+        `
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+};
+
+export default config;
+`.trim(),
+        'utf8'
+      );
+    }
+
+    const { setupStyles } = await prompts({
+      name: 'setupStyles',
+      type: 'confirm',
+      message: 'Do you want to set up the Bits of Good theme?',
+      initial: true,
+    });
+
+    if (!setupStyles) {
+      console.error('Skipping the Bits of Good theme setup. Your project may not look like the Design System Website.');
+      if (setupTailwind) {
+        console.error(
+          'You will need to finish the Tailwind setup manually. Create a css file with `@import "tailwindcss"` in it, and make sure you import it into your src/app/layout.tsx or src/pages/_app.tsx.'
+        );
+      }
+    } else {
+      const { stylePath } = await prompts({
+        name: 'stylePath',
+        type: 'text',
+        message: 'Where should the BOG theme and tailwindcss stylesheet live?',
+        initial: 'src/styles/globals.css',
+      });
+
+      const response = await fetch(
+        'https://raw.githubusercontent.com/GTBitsOfGood/design-system/refs/heads/main/src/styles/globals.css'
+      );
+      const styles = await response.text();
+
+      await mkdir(path.dirname(stylePath), { recursive: true });
+      await writeFile(stylePath, styles, 'utf8');
+      console.log('BOG theme and tailwindcss stylesheet created.');
+      console.log('make sure to import it into your src/app/layout.tsx or src/pages/_app.tsx');
+    }
   });
