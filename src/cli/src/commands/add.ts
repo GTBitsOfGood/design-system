14b75a6419552e26a8be494e837aa5f11628d3cf
minimatime <nvalluri3@gatech.edu>
2025-03-02 16:52:30 -0500

    rebased from main and added 2 components

--- src/cli/src/commands/add.ts
+++ src/cli/src/commands/add.ts
@@ -3,7 +3,7 @@ import fs, { existsSync } from 'fs';
 import path from 'path';
 import prompts from 'prompts';
 
-const COMPONENTS = ['checkbox', 'radio-group', 'radio-item'];
+const COMPONENTS = ['checkbox', 'radio-group', 'radio-item', 'button', 'icon'];
 
 export const add = new Command()
   .command('add')
