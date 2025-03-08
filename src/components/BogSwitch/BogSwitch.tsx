89c45cb17aa29b41e8f046e5d77805dceaa04f74
jpise3 <63403110+jaipise@users.noreply.github.com>
2025-03-08 14:49:11 -0500

    incorporated review comments - fixed styles + got rid of cl sx uses

--- src/components/BogSwitch/BogSwitch.tsx
+++ src/components/BogSwitch/BogSwitch.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as RadixSwitch from '@radix-ui/react-switch';
 import styles from './styles.module.css';
-import clsx from 'clsx';
 
 export interface BogSwitchProps extends React.ComponentPropsWithoutRef<typeof RadixSwitch.Root> {
   label?: string;
@@ -9,26 +8,25 @@ export interface BogSwitchProps extends React.ComponentPropsWithoutRef<typeof Ra
 }
 
 export const BogSwitch: React.FC<BogSwitchProps> = ({ label, size = 'medium', className, style, ...props }) => {
+  const containerClass = `${styles['bog-switch-container']} ${className || ''}`.trim();
+  const rootClass = `${styles['bog-switch']} ${
+    size === 'small'
+      ? styles['bog-switch-small']
+      : size === 'medium'
+        ? styles['bog-switch-medium']
+        : styles['bog-switch-large']
+  }`.trim();
+  const thumbClass = `${styles['bog-switch-thumb']} ${
+    size === 'small'
+      ? styles['bog-switch-thumb-small']
+      : size === 'medium'
+        ? styles['bog-switch-thumb-medium']
+        : styles['bog-switch-thumb-large']
+  }`.trim();
   return (
-    <div className={clsx(styles['bog-switch-container'], className)} style={style}>
-      <RadixSwitch.Root
-        className={clsx(
-          styles['bog-switch'],
-
-          size === 'small' && styles['bog-switch-small'],
-          size === 'medium' && styles['bog-switch-medium'],
-          size === 'large' && styles['bog-switch-large']
-        )}
-        {...props}
-      >
-        <RadixSwitch.Thumb
-          className={clsx(
-            styles['bog-switch-thumb'],
-            size === 'small' && styles['bog-switch-thumb-small'],
-            size === 'medium' && styles['bog-switch-thumb-medium'],
-            size === 'large' && styles['bog-switch-thumb-large']
-          )}
-        />
+    <div className={containerClass} style={style}>
+      <RadixSwitch.Root className={rootClass} {...props}>
+        <RadixSwitch.Thumb className={thumbClass} />
       </RadixSwitch.Root>
       {label && <span className={styles['bog-switch-label']}>{label}</span>}
     </div>
