cf754250664503405b46bac77a7f2fa67c7af012
npapa3 <npapa3@gatech.edu>
2025-10-28 00:51:47 -0400

    Add story for popover

---
+++ src/stories/BogPopover.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import BogChip from '../components/BogChip/BogChip';
+import BogPopover from '../components/BogPopover/BogPopover';
+import BogIcon from '../components/BogIcon/BogIcon';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Popover',
+  component: BogPopover,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+} satisfies Meta<typeof BogPopover>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Popover: Story = {
+  args: {
+    trigger: <BogChip color="cyan">Click me</BogChip>,
+    content: (
+      <div>
+        <p>Hello, I am a popover!</p>
+        <span>Additional content...</span>
+      </div>
+    ),
+  },
+};
